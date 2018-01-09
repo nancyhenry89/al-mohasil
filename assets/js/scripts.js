05126d9d079b4c04ea0585062ebe54ab4b9f451b
Nancy <nancyhenry89@gmail.com>
2018-01-09 10:29:04 +0200

    About page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,6 @@
 $(function() {
-  $('.angel-down').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
   });
 });
