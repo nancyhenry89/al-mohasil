07f99c701fcbb2281ed6842c5d8abf47e296c1dd
Nancy <nancyhenry89@gmail.com>
2018-01-12 21:56:56 +0200

    Express invoice popups partial

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -72,3 +72,13 @@ function authenticate($username, $password) {
     });
 
 }
+//auto close modal
+$(function(){
+    $('#send-invoice').on('show.bs.modal', function(){
+        var myModal = $(this);
+        clearTimeout(myModal.data('hideInterval'));
+        myModal.data('hideInterval', setTimeout(function(){
+            myModal.modal('hide');
+        }, 3000));
+    });
+});
